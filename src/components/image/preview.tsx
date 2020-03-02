8cc81826ecadba47382c3f8c676c6b1388ba8b66
zhangming <qianzm2490@vip.qq.com>
2020-03-02 20:50:58 +0800

    fix: fix build error

--- src/components/image/preview.tsx
+++ src/components/image/preview.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import { ConfigContext, ConfigConsumerProps } from '../config-provider';
 import {
